df4081c2174f4d597cfea863f08255dccd505bad
Marek Grzyb <grzybdev@gmail.com>
2022-12-23 13:04:23 +0100

    Added constants

---
+++ kingdomheartstools/common/constants.py
@@ -0,0 +1,23 @@
+languages = ["", "jp", "us", "us", "it", "sp", "gr", "fr", "jp"]
+
+worlds = [
+    "zz",
+    "es",
+    "tt",
+    "di",
+    "hb",
+    "bb",
+    "he",
+    "al",
+    "mu",
+    "po",
+    "lk",
+    "lm",
+    "dc",
+    "wi",
+    "nm",
+    "wm",
+    "ca",
+    "tr",
+    "eh",
+]
